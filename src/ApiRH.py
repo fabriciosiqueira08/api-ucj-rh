84ee3abed63c19cd97d39174646e5f9c27b9c593
Fabricio Siqueira <114695393+fabriciosiqueira08@users.noreply.github.com>
2024-04-18 14:31:09 -0300

    feat: Finalizado planilha ENPS

--- src/ApiRH.py
+++ src/ApiRH.py
@@ -1,21 +1,18 @@
 import requests
-from openpyxl import Workbook
-from openpyxl.styles import Font, Alignment, Border, Side, PatternFill
+from openpyxl import Workbook, load_workbook
+from openpyxl.styles import Font, Alignment, Border, Side
 from openpyxl.utils import get_column_letter
 
 # Definições
 PIPEFY_API_TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJQaXBlZnkiLCJpYXQiOjE3MTE3MTY3MzgsImp0aSI6IjMwYzBiNjMzLTg5ZWEtNDM0ZC1hODMxLTZkYjc3OTM0MTBkNiIsInN1YiI6MjEyODA2LCJ1c2VyIjp7ImlkIjoyMTI4MDYsImVtYWlsIjoicmhAdWNqLmNvbS5iciIsImFwcGxpY2F0aW9uIjozMDAzMzgyMzIsInNjb3BlcyI6W119LCJpbnRlcmZhY2VfdXVpZCI6bnVsbH0.79e7athW43b4WrBvWOsxa4wsIEUbQlVRzdU6rlZ4pmjDB2ABiv8sOyPu0jv18Gj5HCkue4QIMavqAqE2CnMHiQ"
 PIPEFY_GRAPHQL_ENDPOINT = 'https://api.pipefy.com/graphql'
-EXCEL_FILENAME = 'Dados_Pipe_RH_ENPS.xlsx'
+PIPE_TO_FILE = {
+    'RH - E-NPS': ('[DRH] ENPS.xlsx', 'Banco de dados'),
+}
 
-# Cabeçalhos para as colunas do Excel
-HEADERS = ['ID do Pipe', 'Nome do Pipe', 'Nome da Fase', 'ID do Card', 'Título do Card', 'Nome do Campo', 'Valor do Campo']
-
-# IDs dos Pipes (substitua pelos IDs reais dos seus pipes)
+# IDs dos Pipes 
 PIPE_IDS = {
     'RH - E-NPS': '301823995',
-    'RH - Matriz de Cursos': '301682389',
-    'RH - Painel Controle Membros': '301654957'
 }
 
 # Função para consultar os dados do Pipefy
@@ -43,21 +40,16 @@ def fetch_pipefy_data(pipe_id):
 
 
     headers = {'Authorization': f'Bearer {PIPEFY_API_TOKEN}'}
-    print(f"Buscando dados para o pipe ID: {pipe_id}")
     response = requests.post(PIPEFY_GRAPHQL_ENDPOINT, json={'query': query}, headers=headers)
     data = response.json()
-    print(f"Resposta da API para o pipe {pipe_id}: {data}")
     return data 
 
 # Função para criar/atualizar o arquivo do Excel com os dados do Pipefy
 def update_excel(wb, data, sheet_name):
     if sheet_name in wb.sheetnames:
         ws = wb[sheet_name]
-        ws.delete_rows(2, ws.max_row)
     else:
         ws = wb.create_sheet(title=sheet_name)
-
-    print(f"Aba '{sheet_name}' selecionada ou criada.")
 
     # Lista de títulos para os cabeçalhos conforme especificado
     headers = [
@@ -104,25 +96,48 @@ def update_excel(wb, data, sheet_name):
 
 
     for phase in data['data']['pipe']['phases']:
+        if "Tri" in phase['name']:
+            num_trimestre = phase['name'].split()[0]  # Assume que o formato é "X° Tri YYYY"
+            formatted_name = f"{num_trimestre} Trimestre"
+        else:
+            formatted_name = phase['name']
         if phase['name'] not in ["Caixa de entrada", "Concluído"]:
             for card_edge in phase['cards']['edges']:
                 card = card_edge['node']
                 field_values = {header: "" for header in headers}  # Inicializa todos os campos com string vazia
-                field_values["Período:"] = phase['name']
-                field_values["NPS"] = card['title']
+                field_values["Período:"] = formatted_name
+
+                try:
+                    field_values["NPS"] = int(card['title'])
+                except ValueError:
+                    try:
+                        field_values["NPS"] = float(card['title'])
+                    except ValueError:
+                        field_values["NPS"] = card['title'] 
 
                 for field in card['fields']:
                     header_name = next((h for h in headers if h.endswith(field['name'] + ":")), None)
                     if field['name'] in headers:
-                        field_values[field['name']] = field['value']  # Preenche os valores onde o cabeçalho coincide com o nome do campo
+                        # Convertendo para número se possível, senão mantém como string
+                        try: 
+                            field_values[field['name']] = int(field['value']) if field['value'].isdigit() else float(field['value'])
+                        except ValueError:
+                            field_values[field['name']] = field['value']  # Preenche os valores onde o cabeçalho coincide com o nome do campo
                     elif header_name:
-                        field_values[header_name] = field['value']
+                        try:
+                            field_values[header_name] = int(field['value']) if field['value'].isdigit() else float(field['value'])
+                        except ValueError:
+                            field_values[header_name] = field['value']  
                     elif field['name'] == "Você pertence a algum grupo minoritário?":
                         field_values["Grupo minoritário?"] = field['value']
                     elif field['name'] == "Você é:":
                         field_values["Cargo:"] = field['value']
                     elif field['name'] == "Em uma escala de 0 a 10, o quanto você recomendaria os produtos da UCJ para amigos ou familiares?":
-                        field_values["NPS produtos"] = field['value']
+                        try:
+                             # Certificando-se de que o valor é um inteiro
+                            field_values["NPS produtos"] = int(field['value'])
+                        except ValueError:
+                            field_values["NPS produtos"] = field['value']
                     elif field['name'] == "Comente sobre o que motivou sua resposta.":
                         field_values["O que motivou sua resposta."] = field['value']
                 # Preenche a linha com os valores coletados
@@ -145,44 +160,53 @@ def update_excel(wb, data, sheet_name):
     standard_width = 8.43
     for col in range(5, 20):  # Colunas E(5) até S(19)
         ws.column_dimensions[get_column_letter(col)].width = standard_width
+    
+    column_letter = get_column_letter(2)  # 'B' corresponde ao índice 2
+    desired_width = 9  # Você pode ajustar esse valor conforme necessário
 
-    column_t_index = headers.index("O que motivou sua resposta.") + 1  # Adjust if the header index changes
+    ws.column_dimensions[column_letter].width = desired_width
+
+    column_t_index = headers.index("O que motivou sua resposta.") + 1  
     t_header_value = ws.cell(row=1, column=column_t_index).value
     ws.column_dimensions[get_column_letter(column_t_index)].width = len(t_header_value) + 2
 
-    column_t_index = headers.index("Comente sobre o que motivou essa resposta.") + 1  # Adjust if the header index changes
+    column_t_index = headers.index("Comente sobre o que motivou essa resposta.") + 1  
     t_header_value = ws.cell(row=1, column=column_t_index).value
     ws.column_dimensions[get_column_letter(column_t_index)].width = len(t_header_value) + 2
     
-    column_t_index = headers.index("O que podemos fazer para melhorar enquanto empresa?") + 1  # Adjust if the header index changes
+    column_t_index = headers.index("O que podemos fazer para melhorar enquanto empresa?") + 1  
     t_header_value = ws.cell(row=1, column=column_t_index).value
     ws.column_dimensions[get_column_letter(column_t_index)].width = len(t_header_value) + 2
 
 
-    wb.save(EXCEL_FILENAME)
-    print(f'Arquivo "{EXCEL_FILENAME}" salvo com sucesso.')
+    print(f"Dados atualizados na aba '{sheet_name}'.")
+
+
+
 
 # Função principal para executar o script
 def main():
-    wb = Workbook()
-    wb.remove(wb.active)  # Remove a aba padrão vazia
-
-    for pipe_name, pipe_id in PIPE_IDS.items():
+    for pipe_name, (filename, sheet_name) in PIPE_TO_FILE.items():
         print(f"Iniciando a consulta dos dados do Pipefy para: {pipe_name}")
-        data = fetch_pipefy_data(pipe_id)
-
+        data = fetch_pipefy_data(PIPE_IDS[pipe_name])
+        
         if 'errors' in data:
             print(f"Erro ao fazer a consulta GraphQL para {pipe_name}:")
             for error in data['errors']:
                 print(error['message'])
         else:
-            print(f"Dados do Pipefy obtidos com sucesso para {pipe_name}. Atualizando o arquivo do Excel...")
-            update_excel(wb, data, pipe_name)
+            print(f"Dados do Pipefy obtidos com sucesso para {pipe_name}.")
+            try:
+                wb = load_workbook(filename)
+                print(f"Arquivo '{filename}' carregado com sucesso.")
+            except FileNotFoundError:
+                wb = Workbook()
+                print(f"Arquivo '{filename}' não encontrado, criando novo arquivo.")
+                wb.remove(wb.active)  # Remover a aba padrão vazia
 
-    print(f"Salvando o arquivo '{EXCEL_FILENAME}'.")            
-
-    wb.save(EXCEL_FILENAME)
-    print(f'Arquivo "{EXCEL_FILENAME}" salvo com sucesso com todas as abas.')
+            update_excel(wb, data, sheet_name)  # Passando corretamente o nome da aba
+            wb.save(filename)
+            print(f'Arquivo "{filename}" salvo com sucesso com a aba atualizada.')
 
 if __name__ == "__main__":
     main()
