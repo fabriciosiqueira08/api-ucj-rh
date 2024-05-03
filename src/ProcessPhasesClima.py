9d135094ada862b7d69664f34159df81195f2223
Fabricio Siqueira <114695393+fabriciosiqueira08@users.noreply.github.com>
2024-05-03 14:11:56 -0300

    feat: Feito alterações para puxar os dados de data e incluir na tabela pesquisa de clima

--- src/ProcessPhasesClima.py
+++ src/ProcessPhasesClima.py
@@ -1,5 +1,6 @@
 from openpyxl.styles import Font, Alignment
 from ProcessCardClima import process_card_clima
+from datetime import datetime
 
 
 def process_phases_clima(ws, headers, all_phases, row_num):
@@ -9,19 +10,32 @@ def process_phases_clima(ws, headers, all_phases, row_num):
     alignment_bottom = Alignment(vertical='bottom')
 
     for phase in all_phases:
-        if isinstance(phase, dict):  # Remove a verificação do nome da fase
-            phase_name = phase.get('name', '')  # Obter o nome da fase, com fallback para string vazia se não encontrado
+        if isinstance(phase, dict):  
             for card_edge in phase['cards']['edges']:
                 card = card_edge['node']
                 field_values = process_card_clima(card, headers)
-                field_values['Mês'] = phase_name
 
-                # Mova o preenchimento da planilha para dentro deste loop
-                for col_num, header in enumerate(headers, 1):
-                    cell = ws.cell(row=row_num, column=col_num, value=field_values.get(header, ""))  # Uso seguro de .get com valor padrão
+                # Acessa corretamente o campo `createdAt` dentro do node
+                created_at_str = card.get('createdAt', '')
+                if created_at_str:
+                    try:
+                        created_at = datetime.fromisoformat(created_at_str.replace("Z", "+00:00"))
+                        created_at_formatted = created_at.strftime('%d/%m/%Y')
+                    except ValueError:
+                        created_at_formatted = created_at_str
+                else:
+                    created_at_formatted = ''
+
+                # Escreve o valor `created_at` na coluna "Data:" (coluna 1)
+                ws.cell(row=row_num, column=1, value=created_at_formatted)
+
+
+                # Ajuste a escrita dos outros campos
+                for col_num, header in enumerate(headers[1:], 2):  # Comece da segunda coluna
+                    cell = ws.cell(row=row_num, column=col_num, value=field_values.get(header, ""))
                     cell.font = normal_font
                     cell.alignment = alignment_bottom
 
-                row_num += 1  # Incremento de linha dentro do loop de cards
+                row_num += 1
 
     return ws, row_num
